01c44653d774f1a99fac414d9ef3c1d793a1686c
ktritsh7 <ktriesh7@gmail.com>
2021-05-16 16:54:14 +0530

    Added:List Problem

--- CollectionPracticeProblem.java
+++ CollectionPracticeProblem.java
@@ -1,8 +1,28 @@
 package com.bridgelabz.collection;
 
+import java.util.*;
+
 public class CollectionPracticeProblem {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Collection Practice problem");
+		doList();
+	}
+	
+	private static void doList() {
+		System.out.println("In doList Problem");
+		//Creating List
+		List<String> l = new LinkedList<>();
+		//Adding object to the list
+		l.add("Ritesh");
+		l.add("KT");
+		l.add("Karan");
+		l.add("Vikash");
+		
+		Iterator<String> itr = l.iterator();
+		while(itr.hasNext()) {
+			Object element = itr.next();
+	        System.out.println(element + " ");
+		}
+		System.out.println();
 	}
 }
