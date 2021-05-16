b8806a62e803d4017e032b8d0bf0d567cbec3d72
ktritsh7 <ktriesh7@gmail.com>
2021-05-16 16:48:36 +0530

    Welcome to Collection Practice Problem

---
+++ CollectionPracticeProblem.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.collection;
+
+public class CollectionPracticeProblem {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Collection Practice problem");
+	}
+}
